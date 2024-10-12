4b66a8d0dcbde51e239660789b613272dd401a25
Nigmatjonov Muhammadjon <148071379+zero1max@users.noreply.github.com>
2024-10-12 15:07:58 +0500

    First Commit

---
+++ School 1/pdp.py
@@ -0,0 +1,56 @@
+users = {
+    "teacher": {
+        "1": {
+            'username': 'Musharraf',
+            'password': '1111',
+            'subject': 'Python'
+        },
+        "2": {
+            'username': 'Miss.Shaxlo',
+            'password': '2222',
+            'subject': 'English'
+        },
+        '3': {
+            'username': "G'ayrataka",
+            'password': '3333',
+            'subject': 'Math'
+        }
+    },
+    "students": {
+        "1": {
+            "fullname": "jack",
+            "username": "jacks",
+            "password": 1111,
+            "grade": 4,
+            "class": "10b23",
+            "homeTask": "Decorators"
+        },
+        "2": {
+            "fullname": "zero1max",
+            "username": "zero",
+            "password": 'zero',
+            "grade": 4,
+            "class": "10b23",
+            "homeTask": "Decorators"
+        },
+    }
+}
+
+auth = """
+1. Login
+2. Register
+"""
+
+menu_students = """
+1. My profile
+2. My lesson
+3. My class
+4. HomeTask
+"""
+
+menu_teachers = """
+1. Classes
+2. Students
+3. Baholash
+4. Exit
+"""
